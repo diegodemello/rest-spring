1069831c9f9ae04dc6a49b6543ee1c71dbc747d4
Diego de Mello <diegodemello027@gmail.com>
2025-02-26 22:26:46 -0300

    Criando o produto.

---
+++ src/main/java/br/com/dio/entity/Produto.java
@@ -0,0 +1,48 @@
+package br.com.dio.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private double preco;
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
---
+++ src/main/java/br/com/dio/service/ProdutoService.java
@@ -0,0 +1,26 @@
+package br.com.dio.service;
+
+import br.com.dio.entity.Produto;
+import br.com.dio.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProdutoService {
+    @Autowired
+    private ProdutoRepository repository;
+
+    public Produto save(Produto produto){
+        return repository.save(produto);
+    }
+
+    public Produto findById(Long id){
+        return repository.findById(id).orElse(null);
+    }
+
+    public List<Produto> findAll(){
+        return repository.findAll();
+    }
+}
